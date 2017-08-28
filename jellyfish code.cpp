cd3aca3d744af407fbfe05949a6c8bb38103cfe7
Karthika Kamath <KK945@users.noreply.github.com>
2017-08-28 13:26:27 +0530

    updated a new code

--- jellyfish code.cpp
+++ jellyfish code.cpp
@@ -1,97 +1,127 @@
+/* Name: Karthika Kamath
+   College: VEERMATA JIJABAI TECHNOLOGICAL INSTITUTE (VJTI)
+   Third Year Btech Electronics */
+
+/* sources used: http://opencv-srf.blogspot.in/p/opencv-c-tutorials.html
+				 O'Reilly - Learning OpenCv book */
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 
 using namespace cv;
 using namespace std;
 
-Mat img; Mat img_gray;
-int thresh = 100;
-int max_thresh = 255;
-
-
-/// Function header
-void thresh_callback(int, void* );
-
-int main( int, char** argv )
+int main( int argc, const char** argv )
 {
-  /// Load source image and convert it to gray
-  img = imread("jellyfish.JPG", CV_LOAD_IMAGE_UNCHANGED);
+     Mat img = imread("jellyfish.JPG", CV_LOAD_IMAGE_UNCHANGED); //read the image data in the file "jellyfish.JPG" and store it in 'img'
 
-  /// Convert image to gray and blur it
-  cvtColor( img, img_gray, COLOR_BGR2GRAY );
-  blur( img_gray, img_gray, Size(3,3) );
-
-  /// Create Window
-  const char* image_window = "Image";
-  namedWindow( image_window, WINDOW_AUTOSIZE );
-  imshow( image_window, img );
-// taskbar created to adjust threshold to detect accordingly
-  createTrackbar( " Threshold:", "Image", &thresh, max_thresh, thresh_callback );
-  thresh_callback( 0, 0 );
-
-  waitKey(0);
-  return(0);
-}
-
-void thresh_callback(int, void* )
-{
-  Mat threshold_output;
-  vector<vector<Point> > contours;
-  vector<Vec4i> hierarchy;
-
-  /// Detect edges using Threshold
-  threshold( img_gray, threshold_output, thresh, 255, THRESH_BINARY );
-  /// Find contours
-  findContours( threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-  /// Approximate contours to polygons + get bounding rects and circles
-  vector<vector<Point> > contours_poly( contours.size() );
-  vector<Rect> boundRect( contours.size() );
-  vector<Point2f>center( contours.size() );
-  vector<float>radius( contours.size() );
-
-  for( size_t i = 0; i < contours.size(); i++ )
-     { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-       boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-       minEnclosingCircle( contours_poly[i], center[i], radius[i] );
+     if (img.empty()) //check whether the image is loaded or not
+     {
+          cout << "Error : Image cannot be loaded..!!" << endl;
+          //system("pause"); //wait for a key press
+          return -1;
      }
 
+     
+	 Mat imgH;
+    img.convertTo(imgH, -1, 2, 0); //increase the contrast (double) so that jellyfishes gets little more distinctly separated from the background
 
-  /// Draw polygonal contour + bonding rects + circles
-  Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
-  for( size_t i = 0; i< contours.size(); i++ )
+    
+    //create windows
+    namedWindow("Original Image", CV_WINDOW_AUTOSIZE);
+    namedWindow("High Contrast", CV_WINDOW_AUTOSIZE);
+   
+    //show the image
+    imshow("Original Image", img);
+    imshow("High Contrast", imgH);
+    
+	cvtColor(imgH, imgH, CV_BGR2GRAY); //change the color image to grayscale image
+	namedWindow("GRAY", CV_WINDOW_AUTOSIZE);
+	  imshow("GRAY", imgH);
+
+    
+	  Mat dst;
+ 
+// Set threshold and maxValue
+double thresh = 127;
+double maxValue = 255; 
+ 
+// Binary Threshold
+threshold(imgH,dst, thresh, maxValue, THRESH_BINARY);
+imshow( "threshold", dst );
+
+vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+   findContours( dst, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) ); //find contours
+
+   //erase all the small contours (dots) made of length less than 50 
+   for (vector<vector<Point> >::iterator it = contours.begin(); it!=contours.end(); )
+{
+    if (it->size()<50)
+        it=contours.erase(it);
+    else
+        ++it;
+}
+
+     /// Get the moments
+  vector<Moments> mu(contours.size() );
+  for( int i = 0; i < contours.size(); i++ )
+     { 
+		 mu[i] = moments( contours[i], false );
+  }
+
+  ///  Get the mass centers
+  vector<Point2f> mc( contours.size() );
+  for( int i = 0; i < contours.size(); i++ )
+     { 
+		 mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); 
+  }
+
+
+
+   //draw contours
+   Mat drawing = Mat::zeros( dst.size(), CV_8UC3 );
+  for( int i = 0; i< contours.size(); i++ )
      {
        
-       drawContours( drawing, contours_poly, (int)i, Scalar(0,255,255), 1, 8, vector<Vec4i>(), 0, Point() );
-       
-       circle( drawing, center[i], (int)radius[i], Scalar(255, 255, 0), 2, 8, 0 );
-	   Rect bounding_rect=boundingRect(contours[i]);
-	   rectangle(drawing, bounding_rect,  Scalar(0,255,0),2, 8,0);
-	   //Bounding Box Centroid
-Point center = Point((bounding_rect.x + bounding_rect.width/2), (bounding_rect.y + bounding_rect.height/2));
+       drawContours( drawing, contours, i, Scalar(0,255,255), 2, 8, hierarchy, 0, Point() );
 
-// print it:
-cout<<"Rectangle " <<i<< " Centroid position is at: " << center.x << " " << center.y << endl;
-//mark red cross at centroid by making two intersecting lines at centroid
-line( drawing,
-        Point( center.x -3, center.y +3),
-        Point(center.x +3, center.y - 3),
+	//for marking the centroid of the fishes small areas of contour needs to be neglected to avoid confusion about it being a whole fish or part of fish
+       if (contourArea(contours[i])>50)
+	   {
+	   
+	   //red cross made at the centroid
+	   line( drawing,
+        Point( mc[i].x -3, mc[i].y +3),
+        Point(mc[i].x +3, mc[i].y - 3),
        Scalar( 0, 0, 255 ),
         2,
         8 );
 line( drawing,
-        Point( center.x +3, center.y +3),
-        Point(center.x - 3, center.y-3),
+        Point( mc[i].x +3, mc[i].y +3),
+        Point(mc[i].x - 3, mc[i].y-3),
        Scalar( 0, 0, 255 ),
         2,
         8 );
+	   }
   }
 
   /// Show in a window
   namedWindow( "Detected", WINDOW_AUTOSIZE );
   imshow( "Detected", drawing );
-} 
+  
 
+     waitKey(0); //wait infinite time for a keypress
+
+     destroyWindow("Original Image"); //destroy the windows
+	 destroyWindow("High Contrast");
+	 destroyWindow("GRAY");
+	 destroyWindow("Detected");
+
+
+     return 0;
+}
+
+//problem is faced where two or more jellyfishes are overlapped so they are considered as one big area and thus marks only one centroid
